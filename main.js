b07f8af247ffce611710b44fce370586b7d26758
Nicollet <nicollet.njora05@gmail.com>
2019-01-25 16:05:25 +0300

    Added views

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+"use strict";
+
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
 const port = 8000
 
+require('dotenv').load();
+
 //ApiKey
-const options =  '10af7ad062a21d9c841877f87b7dec3dbe51aeb3';
+const options =  process.env.K2_API_KEY;
 
 //Including the kopokopo module
-var k2 = require("/home/k2-engineering-01/kopokopo_nodejs/index")(options);
-
-
+var k2 = require("/home/k2-engineering-01/Desktop/repos/k2_connect_nodejs/index")(options);
 
 app.use(bodyParser.json())
 app.use(
@@ -18,44 +20,30 @@ app.use(
   })
 ) 
 
-webhooks = k2.webhooks;
+app.set('view engine', 'jade');
 
-app.post('/webhook', webhooks.buygoodsReceived)
+var webhooks = k2.webhooks;
+var resource;
 
+app.post('/webhook', function(req, res, next){
+  resource = webhooks.buygoodsReceived(req,res);
+})
 
+  /* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', res.locals.commonData);
+});
 
-// var options = {
-//   host: "developer.api.autodesk.com",
-//   path: "/oss/v1/buckets",
-//   method: "POST",
-//   headers: {
-//       "Content-Type": "application/json",
-//       "Authorization": "Bearer token"
-//   }
-// };
+/* GET resource page. */
+app.get('/getresource', function(req, res, next) {
 
-// var req = http.request(options, function (res) {
-//   var responseString = "";
-
-//   res.on("data", function (data) {
-//       responseString += data;
-//       // save all the data from response
-//   });
-//   res.on("end", function () {
-//       console.log(responseString); 
-//       // print to console when response ends
-//   });
-// });
-
-// var reqBody = "sometext";
-// req.write(reqBody);
-
-// req.end();
-
-
-// app.get('/resource', k2.Resource.sayHello())
-
-// console.log(k2.Resource.reference);
+  if (resource != null){
+    res.render('resource', {param1: resource.name} );
+  }else{
+    console.log("Resource not yet created")
+    res.render('resource', {param1: "Resource not yet created"} );
+  }  
+});
 
 app.listen(port, () => {
    console.log(`App running on port ${port}.`)
