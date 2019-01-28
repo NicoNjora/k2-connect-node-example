262b4164ace63befaee7050fd9bab20ad162afc0
NicolletNjora <nicollet.njora05@gmail.com>
2019-01-28 15:12:23 +0300

    Ignore node_modules && minor changes on rendering content

--- main.js
+++ main.js
@@ -8,7 +8,13 @@ const port = 8000
 require('dotenv').load();
 
 //ApiKey
-const options =  process.env.K2_API_KEY;
+// const options =  process.env.K2_API_KEY;
+
+const options = {
+  apiKey: process.env.K2_API_KEY,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+};
 
 //Including the kopokopo module
 var k2 = require("/home/k2-engineering-01/Desktop/repos/k2_connect_nodejs/index")(options);
@@ -22,11 +28,11 @@ app.use(
 
 app.set('view engine', 'jade');
 
-var webhooks = k2.webhooks;
+var Webhooks = k2.Webhooks;
 var resource;
 
 app.post('/webhook', function(req, res, next){
-  resource = webhooks.buygoodsReceived(req,res);
+  resource = Webhooks.buygoodsReceived(req,res);
 })
 
   /* GET home page. */
@@ -38,13 +44,37 @@ app.get('/', function(req, res, next) {
 app.get('/getresource', function(req, res, next) {
 
   if (resource != null){
-    res.render('resource', {param1: resource.name} );
+    res.render('resource', {origination_time: resource._origination_time, 
+                            sender_msisdn: resource._sender_msisdn,
+                            amount: resource._amount,
+                            currency: resource._currency,
+                            till_number: resource._till_number,
+                            name: resource.name,
+                            status: resource.status,
+                            system: resource.system
+                            } );
   }else{
     console.log("Resource not yet created")
-    res.render('resource', {param1: "Resource not yet created"} );
+    res.render('resource', {error: "Resource not yet created"} );
   }  
 });
 
+// Use the service
+const subscribeOptions = {
+  event_type: "buygooods_transaction_received",
+  url: "http://localhost/webhook"
+}
+
+// Send message and capture the response or error
+Webhooks.send(subscribeOptions)
+        .then( response => {
+            console.log(response);
+        })
+        .catch( error => {
+            console.log(error);
+        });
+
+        
 app.listen(port, () => {
    console.log(`App running on port ${port}.`)
 })
